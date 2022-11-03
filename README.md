d892e5646d8660359bc1025023e323bd856e26fe
Genevieve Ikechukwu <ikechukwummachukwu2020@gmail.com>
2022-11-03 11:01:25 -0700

    fixed using pandas library

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ A cli tool that generates sha256 of your csv file and appends it to the csv file
 If you want to have a neat work, create folders where your files (manual and auto-generated) will be stored for you.
 
 Make sure to provide the input csv and follow the prompts...
+
+Add the correct path to the cli.py file
--- cli.py
+++ cli.py
@@ -1,21 +1,21 @@
-"""
-Creating a programme to generate a json file and a sha256 and output.csv
+# """
+# Creating a programme to generate a json file and a sha256 and output.csv
 
-Algirithm
+# Algirithm
 
-1. Prompt user to enter a csv input. (not specified in question...skipping.)
-2. Scan and sort through the csv collecting data.
-3. Use the collected data to generate a json file.
-4. Create your sha256 hash from the json file.
-5. Append it to another line in the csv (for each rows).
-6. Return a new csv file titled originalfilename.output.csv with the new line containing the sha256.
+# 1. Prompt user to enter a csv input. (not specified in question...skipping.)
+# 2. Scan and sort through the csv collecting data.
+# 3. Use the collected data to generate a json file.
+# 4. Create your sha256 hash from the json file.
+# 5. Append it to another line in the csv (for each rows).
+# 6. Return a new csv file titled originalfilename.output.csv with the new line containing the sha256.
 
-"""
+# """
 import json
 import csv
 import ast
 
-# importing modules from other scripts.....
+# # importing modules from other scripts.....
 
 
 
@@ -27,29 +27,39 @@ from hashed import hash_json
 
 
 
-def output_csv():
-    read_csv()
-    hash_json()
+# def output_csv():
+#     read_csv()
+#     hash_json()
 
-    filename = input('Enter the path to the appended json, e.g: ./json_output/file.json ')
-    output = input('Enter the path where you want your csv to be stored e.g: ./csv_output/filename.output.csv ')
+#     filename = input('Enter the path to the appended json, e.g: ./json_output/file.json ')
+#     output = input('Enter the path where you want your csv to be stored e.g: ./csv_output/filename.output.csv ')
 
-    with open(filename) as json_file:
-        json_data = json.load(json_file)
-        json_data = str(json_data)
-        eg = eval(json_data)
-        json_csv = eg
-    data_file = open(output, 'w', newline='')
-    csv_writer = csv.writer(data_file)
-    count = 0
+#     with open(filename) as json_file:
+#         json_data = json.load(json_file)
+#         json_data = str(json_data)
+#         eg = eval(json_data)
+#         json_csv = eg
+#     data_file = open(output, 'w', newline='')
+#     csv_writer = csv.writer(data_file)
+#     count = 0
 
-    for data in json_csv:
-        if count == 0:
-            header = data.keys()
-            csv_writer.writerow(header)
-            count += 1
-            csv_writer.writerow(data.values())
-            data_file.close()
-            break
-output_csv()
+#     for data in json_csv:
+#         if count == 0:
+#             header = data.keys()
+#             csv_writer.writerow(header)
+#             count += 1
+#             csv_writer.writerow(data.values())
+#             data_file.close()
+#             break
+# output_csv()
 
+# using pandas library
+import pandas as pd
+read_csv() 
+hash_json()
+
+# path to json file
+df = pd.read_json (r'./json_output/file.json')
+
+# path where it will be saved
+df.to_csv (r'./csv_output/filename.output.csv', index = 1)
